288560120212ef3cda53da8d7880bb45507e2ef5
Margaret Carlin <margaret@screencloud.io>
2019-11-05 13:30:22 +0700

    Get started with slack app

--- back-end/app.ts
+++ back-end/app.ts
@@ -3,10 +3,18 @@ const { App } = require('@slack/bolt');
 
 const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
-  token: process.env.SLACK_BOT_TOKEN,
+  token: process.env.SLACK_BOT_TOKEN
 });
 
 /* Add functionality here */
+app.command("/flash", async ({ command, ack, say }) => {
+  // Acknowledge command request
+  ack();
+  let message = command.text;
+  let commandComponents = command.text.split("\"");
+  say(`Here's the message to broadcast: ${message}`);
+});
+
 
 (async () => {
   // Start the app
