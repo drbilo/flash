5f2780864bc2a03f1d7eec496afab6e607c9f85b
Margaret Carlin <margaret@screencloud.io>
2019-11-05 14:44:46 +0700

    Working now - parse command text

--- back-end/app.js
+++ back-end/app.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const { App } = require('@slack/bolt');
-const { parseFlashCommand } = require("./parseFlashCommand");
+//const { parseFlashCommand } = require("./parseFlashCommand");
 
 const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
@@ -12,7 +12,7 @@ app.command("/flash", async ({ command, ack, say }) => {
   // Acknowledge command request
   ack();
   const message = command.text;
-  const commandComponents = parseFlashCommand(command);
+  const commandComponents = parseFlashCommand(command.text);
 
   say(`Here's the message to broadcast: "${commandComponents.message}" of duration ${commandComponents.duration} seconds with type ${commandComponents.level}`);
 });
@@ -24,3 +24,32 @@ app.command("/flash", async ({ command, ack, say }) => {
 
   console.log('⚡️ Bolt app is running!');
 })();
+
+
+function parseFlashCommand(commandString) {
+  const messageStart = commandString.indexOf('"');
+  const defaultDuration = 1; // minute
+  const defaultLevel = "info";
+
+  let [level, durationRaw] = commandString
+    .substring(0, messageStart)
+    .split(" ");
+
+  const message = commandString.substring(
+    messageStart + 1,
+    commandString.length - 1
+  );
+
+  // set reasonable defaults if missing any part of command
+  if (!level) {
+    level = defaultLevel;
+  }
+  if (!durationRaw) {
+    durationRaw = defaultDuration;
+  }
+
+  // Level is input as minutes and returns as seconds
+  const duration = durationRaw * 60;
+
+  return { level, duration, message };
+}
