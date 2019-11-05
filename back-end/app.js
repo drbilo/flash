77e857811993623a0a27e44cebdac6e851817a35
Macavirus <macavirus@zoho.com>
2019-11-05 15:47:30 +0700

    Add proper auth to graphql request

--- back-end/app.js
+++ back-end/app.js
@@ -50,29 +50,32 @@ app.command("/flash", async ({ command, ack, say }) => {
   // Acknowledge command request
   ack();
   const message = command.text;
-  const commandComponents = parseFlashCommand(command.text);
+  const commandComponents = parseFlashCommand(command);
 
-  // Run GraphQL queries/mutations using a static function
-  // request(endpoint, query, variables).then(data => console.log(data))
-  //
-  // Example:
-  const screencloudGraphqlEndpoint =
-    "https://graphql.eu.screencloud.com/graphql";
+  const endpoint = "https://graphql.eu.screencloud.com/graphql";
+  const studioApiToken =
+    "c6a42c7a-1270-41b1-afb2-f44b85b7d156:eee04829c25ab9cab055fb46f997710c";
   const screenIds = ["75b5cfbf-6294-4650-b70a-e7a8c6c65a95"];
   const teamId = "7887918f-6855-4861-bfdd-dd66bbba1ad3";
   const linkId = "e1c673d9-8324-4133-b1da-b9878c01ed92";
   const priority = 10;
 
-  // graphql library method, try?
-  request(
-    screencloudGraphqlEndpoint,
-    castLinkMutation,
-    castLinkInput(screenIds, teamId, linkId, priority)
-  )
-    .then(data => console.log(data))
-    .catch(e => console.log(e));
-  say(`⚡️Here's the message to broadcast: "${commandComponents.message}" \n ⚡️Duration: ${commandComponents.duration} seconds \n ⚡️ Type: ${commandComponents.level}`);
-});
+  const graphQLClient = new GraphQLClient(endpoint, {
+    headers: {
+      authorization: `Bearer ${studioApiToken}`
+    }
+  });
+
+  // Run mutation to cast
+  try {
+    const data = await graphQLClient.request(
+      castLinkMutation,
+      castLinkInput(screenIds, teamId, linkId, 10)
+    );
+    console.log(JSON.stringify(data, null, 2));
+  } catch (e) {
+    console.log(e);
+  }
 
   say(
     `Here's the message to broadcast: "${commandComponents.message}" of duration ${commandComponents.duration} seconds with type ${commandComponents.level}`
@@ -83,34 +86,5 @@ app.command("/flash", async ({ command, ack, say }) => {
   // Start the app
   await app.start(process.env.PORT || 3000);
 
-  console.log('⚡️ Flash app is running!');
+  console.log("⚡️ Bolt app is running!");
 })();
-
-
-function parseFlashCommand(commandString) {
-  const messageStart = commandString.indexOf('"');
-  const defaultDuration = 1; // minute
-  const defaultLevel = "info";
-
-  let [level, durationRaw] = commandString
-    .substring(0, messageStart)
-    .split(" ");
-
-  const message = commandString.substring(
-    messageStart + 1,
-    commandString.length - 1
-  );
-
-  // set reasonable defaults if missing any part of command
-  if (!level) {
-    level = defaultLevel;
-  }
-  if (!durationRaw) {
-    durationRaw = defaultDuration;
-  }
-
-  // Level is input as minutes and returns as seconds
-  const duration = durationRaw * 60;
-
-  return { level, duration, message };
-}
